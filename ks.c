d193064bcca86c093fbdfd7c1f82114cc272067c
UDMH <149693453+YukkimuraHinata@users.noreply.github.com>
2024-01-22 19:18:00 +0800

    Update ks.c

--- ks.c
+++ ks.c
@@ -20,11 +20,27 @@ void signJudge(int * judge, char * computeSign)
     }
 }
 
+int sumJudge(int * xInput,int * yInput, int * count)
+{
+    int xrecive,yrecive,loopcount;  //use int input to recive z = rand(),int count to recive i
+    xrecive = *xInput;
+    yrecive = *yInput;
+    loopcount = *count;
+    if(xrecive == 0 || yrecive == 0){
+        loopcount--;
+    }
+    if(xrecive == yrecive){
+        loopcount--;
+    }
+    return loopcount;
+}
+
 int main()
 {
-    int i,n,x,y,z,result;  //z = sign, x,y = number  
+    int i,n,x,y,z;  //z = sign, x,y = number  
     char ch;
-    printf_s("输入想要生成的题目数量\n");
+    printf_s("ver1.0,build 24 \n");
+    printf_s("杈撳叆鎯宠鐢熸垚鐨勯鐩暟閲廫n");
     scanf_s("%d",&n);
     int question[n][3];
     char computeSign[n + 1];
@@ -34,33 +50,23 @@ int main()
         z = rand()%4;  // 0 = add, 1 = sub, 2 = multi, 3 = div
         if(z == 2 || z == 3) //multi and division
         {
-            if(z == 2){
+            if(z == 2){  //multi
                 x = rand() % 10;
                 y = rand() % 10;
-                if(x|y == 0){
-                    i--;
-                }
-                else{
                 question[i][0] = x;
                 question[i][1] = y;
                 question[i][2] = x*y;
+                i = sumJudge(&x,&y,&i);
                 signJudge(&z,&computeSign[i]);
-                printf_s("%d %c %d\n",question[i][0],computeSign[i],question[i][1]);
-                }
             }
-            else{
-                if(x|y == 0){
-                    i--;
-                }
-                else{
+            else{  //div
                 x = rand() % 10;
                 y = rand() % 10;
                 question[i][0] = x*y;
                 question[i][1] = x;
                 question[i][2] = y;
+                i = sumJudge(&x,&y,&i);
                 signJudge(&z,&computeSign[i]);
-                printf_s("%d %c %d\n",question[i][0],computeSign[i],question[i][1]);
-                }
             }
         }
         else{
@@ -74,9 +80,13 @@ int main()
             }
             question[i][0] = x;
             question[i][1] = y;
+            i = sumJudge(&x,&y,&i);
             signJudge(&z,&computeSign[i]);
-            printf_s("%d %c %d\n",question[i][0],computeSign[i],question[i][1]);
         }
+    }
+    for(i = 0;i < n;i++)
+    {
+        printf_s("%d %c %d = \n",question[i][0],computeSign[i],question[i][1]);
     }
     fflush(stdin);
     printf_s("Press Enter to check the anwser");
@@ -86,5 +96,5 @@ int main()
         {
             printf_s("%d %c %d = %d\n",question[i][0],computeSign[i],question[i][1],question[i][2]);
         }
-        return 0; 
+        return 0;
 }
